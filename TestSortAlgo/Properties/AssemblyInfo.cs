ef652b132c11ab27dcc48eecd8be3965a75b871a
M-PC\M <nata63726@gmail.com>
2018-09-06 23:20:41 +0900

    Add UnitTest.

---
+++ TestSortAlgo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SortAlgoTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("SortAlgoTest")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91156b74-18c8-4a6e-ab93-e4d73dd4a989")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestSortAlgo/UnitTestOfEachSort.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestSortAlgo
+{
+    [TestClass]
+    public class UnitTestOfEachSort
+    {
+        private void Test_SortAlgo(Func<List<int>, List<int>> SortFunc)
+        {
+            int count = 123;
+
+            List<int> expect = new List<int>();
+            for (int i = 1; i <= count; i++)
+            {
+                expect.Add(i);
+            }
+
+            int[] sourceArray = new int[expect.Count];
+            expect.CopyTo(sourceArray);
+            for (int i = 1; i < sourceArray.Length; i++)
+            {
+                int tmp = sourceArray[i];
+                sourceArray[i] = sourceArray[i - 1];
+                sourceArray[i - 1] = tmp;
+            }
+            List<int> source = new List<int>(sourceArray);
+
+            List<int> actual = SortFunc(source);
+
+            CollectionAssert.AreEqual(expect, actual);
+        }
+
+        [TestMethod]
+        public void Test_Babble()
+        {
+            Test_SortAlgo(SortAlgo.SortAlgos.BabbleSort);
+        }
+
+        [TestMethod]
+        public void Test_Shaker()
+        {
+            Test_SortAlgo(SortAlgo.SortAlgos.ShakerSort);
+        }
+
+        [TestMethod]
+        public void Test_Comb()
+        {
+            Test_SortAlgo(SortAlgo.SortAlgos.CombSort);
+        }
+
+        [TestMethod]
+        public void Test_Selection()
+        {
+            Test_SortAlgo(SortAlgo.SortAlgos.SelectionSort);
+        }
+
+        [TestMethod]
+        public void Test_Insertion()
+        {
+            Test_SortAlgo(SortAlgo.SortAlgos.InsertionSort);
+        }
+
+        [TestMethod]
+        public void Test_Shell()
+        {
+            Test_SortAlgo(SortAlgo.SortAlgos.ShellSort);
+            Test_SortAlgo(SortAlgo.SortAlgos.ShellSort2);
+        }
+
+        [TestMethod]
+        public void Test_Merge()
+        {
+            Test_SortAlgo(SortAlgo.SortAlgos.MergeSort);
+        }
+    }
+}
